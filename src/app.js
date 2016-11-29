043d37b9476116291b2635c4d0f624899abc98c1
Trish Griego <pmgriego@gmail.com>
2016-11-28 20:06:12 -0800

    Added functionality for waves 1-3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
+//app.js
+// Main entry-point of the application.
+
 import $ from 'jquery';
 import _ from 'underscore';
 import ApplicationView from 'app/views/application_view';
+
+var quoteData = [
+  {
+    symbol: 'HUMOR',
+    price: 88.50
+  }, {
+    symbol: 'CLOTH',
+    price: 81.70
+  }, {
+    symbol: 'HABIT',
+    price: 98.00
+  }, {
+    symbol: 'SUPER',
+    price: 83.10
+  }, {
+    symbol: 'INGRD',
+    price: 79.40
+  }, {
+    symbol: 'MXTPE',
+    price: 109.20
+  }, {
+    symbol: 'CNTAR',
+    price: 90.70
+  }, {
+    symbol: 'EVCOLR',
+    price: 101.90
+  }, {
+    symbol: 'FUZZY',
+    price: 88.60
+  }
+];
+
 
 const simulate = function(quote) {
   // Calculate a random price movement
@@ -19,12 +54,13 @@ const simulate = function(quote) {
 
 $(document).ready(function() {
   var appView = new ApplicationView({
-    el: '#application'
+    el: $('#application'),
+    quoteData: quoteData
   });
 
   appView.render();
 
-  setInterval(function() {
-    // Call simulate() on each quote in the ApplicationView
-  }, 1000);
+  // setInterval(function() {
+  //   // Call simulate() on each quote in the ApplicationView
+  // }, 1000);
 });
--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -1,14 +1,58 @@
+// application_view.js
+// equivalent of task_list_view.js
+
+import $ from 'jquery';
 import Backbone from 'backbone';
+import _ from 'underscore';
+
+import QuoteView from 'app/views/quote_view';
 
 const ApplicationView = Backbone.View.extend({
-  initialize: function() {
+  initialize: function(options) {
+    // Store a the full list of quotes
+    this.quoteData = options.quoteData;
+
+    // Compile a template to be shared between the individual quotes
+    this.quoteTemplate = _.template($('#tmpl-quote-view').html());
+
+    // Keep track of the <ul> element
+    // almost got this wrong w/ using application
+    this.listElement = this.$('.quotes');
+
+    // Create a QuoteView for each task
+      this.cardList = [];
+      this.cardList = [];
+   this.quoteData.forEach(function(quote) {
+     var card = new QuoteView({
+       quote: quote,
+       template: this.quoteTemplate
+     });
+     this.cardList.push(card);
+   }, this); // bind `this` so it's available inside forEach
+
+
+    // this.input = {
+    //   title: this.$('.new-quote input[name="title"]'),
+    //   description: this.$('new-quote input[name="description"]')
+    // };
+
   },
 
   render: function() {
-    this.$el.html('<p>Hello World</p>');
+    // we loop through the list of TaskViews, render each one and append the result to our DOM object.
+    this.listElement.empty();
+
+    // loop through the data assigned to this view.
+    this.cardList.forEach(function(card){
+      card.render();
+      // Add that HTML to our task list
+      // referencing jquery object above.
+      this.listElement.append(card.$el);
+    }, this);
 
     return this;
-  }
+  },
+
 });
 
 export default ApplicationView;
